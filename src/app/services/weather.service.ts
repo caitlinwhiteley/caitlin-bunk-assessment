edfd13eac06e0a2685301b89465861264c9d1261
Caitlin Whiteley <Caitlin.Whiteley@nology.io>
2019-01-14 15:19:36 +0000

    content and functionality of weather section

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { ICity } from '../weather/weather.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  urlStart: string;
+  apiKey: string;
+  urlEnd: string;
+
+  constructor(private http: HttpClient) {
+    this.urlStart = 'https://api.openweathermap.org/data/2.5/weather?id=';
+    this.apiKey = 'f4bbeb8ec1a8fb95591f2cf112aaa575'
+    this.urlEnd = '&appid=' + this.apiKey;
+  }
+
+  getConfig(id: number) {
+    return this.http.get(this.urlStart + id + this.urlEnd);
+  }
+
+  getResults(response): ICity {
+    const cityDetails = {
+      city: response.name,
+      temp: this.convertKelvin(response.main.temp),
+      humidity: response.main.humidity,
+      minTemp: this.convertKelvin(response.main.temp_min),
+      maxTemp: this.convertKelvin(response.main.temp_max)
+    }
+    return cityDetails;
+  }
+
+  convertKelvin(kelvin: number): number {
+    const celcius = (kelvin - 273.15).toFixed(2);
+    return Number(celcius);
+  }
+
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
+
+export interface ICity {
+  city: string,
+  temp: number,
+  humidity: number,
+  minTemp: number,
+  maxTemp: number
+}
 
 @Component({
   selector: 'app-weather',
@@ -6,10 +15,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
+  london: ICity;
+  rome: ICity;
+  bristol: ICity;
 
-  constructor() { }
+  londonID: number = 2643744;
+  romeID: number = 4219762;
+  bristolID: number = 4749005;
 
-  ngOnInit() {
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(){
+    this.weatherService.getConfig(this.londonID).subscribe((response) => {
+      this.london = this.weatherService.getResults(response);
+    })
+    this.weatherService.getConfig(this.bristolID).subscribe((response) => {
+      this.bristol = this.weatherService.getResults(response);
+    })
+    this.weatherService.getConfig(this.romeID).subscribe((response) => {
+      this.rome = this.weatherService.getResults(response);
+    })
   }
+
+  
 
 }
