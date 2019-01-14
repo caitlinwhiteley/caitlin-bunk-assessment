2b3576e3af8ac180eb1be3e007a15f88190c7437
Caitlin Whiteley <Caitlin.Whiteley@nology.io>
2019-01-14 14:13:22 +0000

    created all the components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { FootballComponent } from './football/football.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherComponent,
+    QuizComponent,
+    FootballComponent
   ],
   imports: [
     BrowserModule
